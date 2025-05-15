bfc39ccf61450a3e8d37ac24157620557d222266
sheetala0 <sheetalahegde39@gmail.com>
2025-05-15 19:47:51 +0530

    this is evenodd program

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd
+
+# Input from the user
+num = int(input("Enter a number: "))
+
+# Check condition
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
